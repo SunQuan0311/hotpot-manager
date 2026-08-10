c44a9678feb298bf3a1764ff95ab96072791d77c
SunQuan0311 <540653801@qq.com>
2026-08-10 10:54:27 +0800

    Disable caching

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='hotpot-v1';const URLS=['./','index.html','manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+// Service Worker - disabled for now
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
